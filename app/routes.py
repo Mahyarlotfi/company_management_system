fc0b55a33bdffdfb0fe8709361c670a87e756ead
mahyarlotfi <mahyarlotfi@gmail.com>
2020-03-15 01:12:29 +0330

    add login page special thanks to BlackrockDigital. ref: https://github.com/BlackrockDigital/startbootstrap-sb-admin-2

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,13 @@
+from flask import render_template
 from app import app
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, Worlds!"
+    return 'Hello'
