5a620f7dc30f36e5171c5a3bdad831ba70ca689e
mahyarlotfi <mahyarlotfi@gmail.com>
2020-03-16 03:19:06 +0330

    create login form

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,16 @@
 from flask import render_template
 from app import app
+from app.forms import LoginForm
 
-@app.route('/login')
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
-
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('login.html'))
+    return render_template('login.html', form=form)
 
 
 @app.route('/')
